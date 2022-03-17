c8f46871fc0005ee334ab8bc4300426dc7c9aff8
gabriel <gabitorres64@gmail.com>
2022-03-17 16:02:28 -0300

    terminamos el componente de lista de tareas icono agregado

--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -3,6 +3,8 @@
 import { Component, OnInit,Input } from '@angular/core'; /**para que pueda recibir cosas del padre debemos importar el input */
 import { Task } from 'src/app/Task'; /**tambien debemos importar el Task para que pueda ser de tipo Task el item que traemos */
 import { TASKS } from 'src/app/mock.tasks'; /**TAMBIEN NECESITAMOS EL tasks PARA INICIALIZAR EL task del @ input con algo o nos dara error */
+import { faTimes } from '@fortawesome/free-solid-svg-icons'; /**asi importamos el icono de la fontawesome ahora nos iremos a tasks-item.html a meter el icono a la etiqueta h3 */
+
 @Component({
   selector: 'app-tasks-item',
   templateUrl: './tasks-item.component.html',
@@ -12,6 +14,7 @@ export class TasksItemComponent implements OnInit {
 
   @Input() task:Task= TASKS[0];    /**asi hago que pueda recibir el item[task] del padre, esta taks sera del tipo Task (es para  lo de la interface),INICIALIZAMOS EL task con TASKS[0] porque si no nos da error de que task no esta declarada */
 
+  faTimes=faTimes; /**creo variable para guardar el icono y le doy el mismo nombre que el icono */
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/Task';  /**IMPORTAMOS LA INTERFACE  */
 import { TASKS } from 'src/app/mock.tasks'; /**IMPORTAMOS EL ARRAY TASKS */
 
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
@@ -10,7 +11,7 @@ import { TASKS } from 'src/app/mock.tasks'; /**IMPORTAMOS EL ARRAY TASKS */
 export class TasksComponent implements OnInit {
 
   tasks:Task[] = TASKS; /**creamos un array tasks que sera del tipo array Task(este Task es la interface que sirve para controlar los tipos de datos) y le decimos que sera igual al array TASKS que ya creamos en mosk.tasks.ts ) */
-
+  
   constructor() { }
 
   ngOnInit(): void {
